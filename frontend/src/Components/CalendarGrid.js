bd5c3f61961be7d0244edbe0068154458f3d8511
JaxToh <117418651+JaxToh@users.noreply.github.com>
2022-12-16 09:29:58 +0800

    added calendar with mock events

--- frontend/src/Components/CalendarGrid.js
+++ frontend/src/Components/CalendarGrid.js
@@ -1,104 +1,144 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+////consts////
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const startingDate = new Date();
+
+////functions & utilities////
+const range = (end) => {
+  const { result } = Array.from({ length: end }).reduce(
+    ({ result, current }) => ({
+      result: [...result, current],
+      current: current + 1,
+    }),
+    { result: [], current: 1 }
+  );
+  return result;
+};
+
+const getDaysOfMonth = (month, year) => {
+  return new Date(year, month + 1, 0).getDate();
+};
+
+const getSortedDays = (month, year) => {
+  const dayIndex = new Date(year, month, 1).getDay();
+  return [...DAYS.slice(dayIndex), ...DAYS.slice(0, dayIndex)];
+};
+
+const getDateObj = (day, month, year) => {
+  return new Date(year, month, day);
+};
+
+const areDatesTheSame = (date1, date2) => {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  );
+};
+
+////calendar////
 function CalendarGrid() {
-  let month = 12;
-  let year = 2022;
+  const [currentMonth, setCurrentMonth] = useState(startingDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(startingDate.getFullYear());
+  const [eventsArr, setEventsArr] = useState([
+    {
+      customer: "John",
+      date: new Date(2022, 11, 12, 13),
+    },
 
-  const MONTHS = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    {
+      customer: "Peter",
+      date: new Date(2022, 11, 20, 15)
+    },
 
-  const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    {
+      customer: "Mary",
+      date: new Date(2022, 11, 23, 13)
+    },
+  ]);
 
-  const selectedDate = new Date(year, month - 1);
-  const [date, setDate] = useState(selectedDate);
+  const DAYSINMONTH = getDaysOfMonth(currentMonth, currentYear);
 
-  const Navigation = ({ date, setDate }) => {
-    return (
-      <div className="navigation">
-        <div
-          className="back"
-          onClick={() => {
-            const newDate = new Date(date);
-            newDate.setMonth(newDate.getMonth() - 1);
-            setDate(newDate);
-          }}
-        >
-          {"<-"} {MONTHS[date.getMonth() === 0 ? 11 : date.getMonth() - 1]}
-        </div>
-
-        <div className="monthAndYear">
-          {MONTHS[date.getMonth()]} {date.getFullYear()}
-        </div>
-
-        <div
-          className="forward"
-          onClick={() => {
-            const newDate = new Date(date);
-            newDate.setMonth(newDate.getMonth() + 1);
-            setDate(newDate);
-          }}
-        >
-          {MONTHS[date.getMonth() === 11 ? 0 : date.getMonth() + 1]} {"->"}
-        </div>
-      </div>
-    );
-  };
-
-  const Days = () => {
-    return DAYS.map((day) => {
-      return (
-        <div className="day cell" key={day}>
-          {day}
-        </div>
-      );
-    });
-  };
-
-  const Grid = ({ date }) => {
-    const ROWS_COUNT = 6;
-
-    const startingDate = new Date(date.getFullYear(), date.getMonth(), 1);
-    startingDate.setDate(startingDate.getDate() - (startingDate.getDay() - 1));
-
-    const dates = [];
-    for (let i = 0; i < ROWS_COUNT * 7; i++) {
-      const date = new Date(startingDate);
-      dates.push({ date });
-      startingDate.setDate(startingDate.getDate() + 1);
+  const nextMonth = () => {
+    if (currentMonth < 11) {
+      setCurrentMonth((prev) => prev + 1);
+    } else {
+      setCurrentMonth(0);
+      setCurrentYear((prev) => prev + 1);
     }
+  };
 
-    return (
-      <>
-        {dates.map((date, i) => {
-          return (
-            <div key={i} className="cell">
-              <div className="date">{date.date.getDate()}</div>
-            </div>
-          );
-        })}
-      </>
-    );
+  const prevMonth = () => {
+    if (currentMonth > 0) {
+      setCurrentMonth((prev) => prev - 1);
+    } else {
+      setCurrentMonth(11);
+      setCurrentYear((prev) => prev - 1);
+    }
   };
 
   return (
-    <div className="calendargrid">
-      <Navigation date={date} setDate={setDate} />
+    <div className="wrapper">
+      <div className="calendarHead">
+        <p onClick={prevMonth}>prev--</p>
+        <p>
+          {MONTHS[currentMonth]} {currentYear}
+        </p>
+        <p onClick={nextMonth}>--next</p>
+      </div>
+      <div className="grid">
+        {getSortedDays(currentMonth, currentYear).map((day, i) => (
+          <span key={i} className="daysHeader">
+            {day}
+          </span>
+        ))}
+      </div>
+      <div className="calendarBody">
+        {range(DAYSINMONTH).map((day, i) => (
+          <span
+            key={i}
+            className={
+              areDatesTheSame(
+                new Date(),
+                getDateObj(day, currentMonth, currentYear)
+              )
+                ? "today"
+                : "days"
+            }
+          >
+            <p>{day}</p>
 
-      <Days />
-
-      <Grid date={date} actualDate={date} />
+            {eventsArr.map(
+              (event) =>
+                areDatesTheSame(
+                  getDateObj(day, currentMonth, currentYear),
+                  event.date
+                ) && (
+                  <span key={event.id} className="events">
+                    {event.customer}
+                  </span>
+                )
+            )}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
